05a38a8137b650a527fe54e8a523a73601d4bbf6
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-02-15 13:46:40 +0300

    Complete array implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import linear_structures.Array;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Array items = new Array(5);
+        items.add(10);
+        items.add(15);
+        items.add(22);
+        items.add(35);
+        items.add(45);
+        items.add(55);
+        items.add(88);
+        items.add(99);
+        items.add(77);
+        items.add(66);
+
+
+        items.print();
+
+        items.removeAt(1);
+        items.removeAt(4);
+        items.removeAt(items.size() - 1);
+        items.removeAt(items.size() - 1);
+        items.print();
+        System.out.println(items.size());
+
 
     }
 }
--- src/main/java/linear_structures/Array.java
+++ src/main/java/linear_structures/Array.java
@@ -1,5 +1,7 @@
 package linear_structures;
 
+import java.util.Arrays;
+
 public class Array {
     private int[] items;
     private int count;
@@ -8,42 +10,58 @@ public class Array {
         items = new int[size];
     }
 
-    public void insert(int item) {
+    public void add(int item) {
         /**
          * Check if the array is full,
-         * If its full -> create a nother arrray double its size
+         * If its full -> create another array double its size
          * Copy all the elements from the old array to the new array
          * Add the new item to the new array
          **/
-
         if (count == items.length) {
             int[] newItems = new int[items.length * 2];
-
-            System.arraycopy(
-                items, 0,
-                newItems, 0,
-                count
-            );
-            items = newItems;
+            System.arraycopy(items, 0, newItems, 0, count);
             newItems[count++] = item;
+            items = newItems;
+            return;
         }
 
         items[count++] = item;
     }
 
-    public int remove( int index) {
-        return 0;
+    public void removeAt( int index) {
+        if(index < 0 || index >= count)
+            throw new IllegalArgumentException("Index out of bounds");
+
+        for(int i = index; i < count - 1  ; i++) {
+            items[i] = items[i + 1];
+        }
+
+        items = reduceArraySize();
+
+        count--;
     }
 
-    public int indexOf(int item){
-        return 0;
+    public int indexOf(int value) {
+        for (int i = 0; i < count; i++) {
+            if (items[i] == value) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     public void print (){
-
+        System.out.println(Arrays.toString(items));
     }
 
     public int size (){
         return count;
+    }
+
+    private int[] reduceArraySize() {
+        var newItems = new int[count - 1];
+        System.arraycopy(items, 0, newItems, 0, count - 1);
+        return newItems;
     }
 }
